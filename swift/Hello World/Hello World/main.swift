451e7ed31baebedd76f5c8712e84a89a8f5e0743
Chris Fuller <chris@cjwfuller.com>
2014-07-28 11:00:38 +0100

    add swift hello world program

---
+++ swift/Hello World/Hello World/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hello World
+//
+//  Created by Chris on 28/07/2014.
+//  Copyright (c) 2014 Chris. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
