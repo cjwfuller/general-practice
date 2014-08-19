948883230746be0d3492bd84de1d212ee51da110
Chris Fuller <chris@cjwfuller.com>
2014-08-19 21:12:36 +0100

    fix BST inorder traversal

--- python/searching/bst.py
+++ python/searching/bst.py
@@ -8,9 +8,8 @@ class BST:
 
     def inorder(self, node):
         """Perform in-order traversal of BST"""
-        if node is not None:
-            for elem in self.inorder(node.left):
-                yield elem
-            yield node.data
-            for elem in self.inorder(node.right):
-                yield elem
+        if node.left is not None:
+            self.inorder(node.left)
+        yield node.data
+        if node.right is not None:
+            self.inorder(node.right)
