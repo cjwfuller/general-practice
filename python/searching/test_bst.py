3ca354812f7e4bf7c204206cc2496f6f867aa4f5
Chris Fuller <chris@cjwfuller.com>
2014-08-20 20:10:28 +0100

    add more comprehensive (failing) inorder traversal tests

--- python/searching/test_bst.py
+++ python/searching/test_bst.py
@@ -3,13 +3,32 @@ import bst
 
 class TestBST(unittest.TestCase):
     def test_basic_insert(self):
-        tree = bst.BST(5, bst.BST(4))
+        tree = bst.BST(5, bst.BST(4), None)
 
-    def test_single_element_inorder(self):
-        tree = bst.BST(4, None, None)
+    def test_left_element_inorder(self):
+        tree = bst.BST(4, 3, None)
         results = tree.inorder(tree)
-        for r in results:
-            self.assertEquals(4, r)
+        answer = [3, 4]
+        count = 0
+        for result in results:
+            self.assertEquals(result, answer[count])
+
+    def test_right_element_inorder(self):
+        tree = bst.BST(5, None, 6)
+        results = tree.inorder(tree)
+        answer = [5, 6]
+        count = 0
+        for result in results:
+            self.assertEquals(result, answer[count])
+
+    def test_basic_inorder(self):
+        tree = bst.BST(7, bst.BST(1, None, None), bst.BST(9, None, None))
+        results = tree.inorder(tree)
+        answer = [1, 7, 9]
+        count = 0
+        for result in results:
+            self.assertEquals(result, answer[count])
+            count = count + 1
 
 if __name__ == '__main__':
     unittest.main()
