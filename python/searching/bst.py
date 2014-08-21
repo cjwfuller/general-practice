d9b8217fbdb747fd73487dec4e20f313cf047ae9
Chris Fuller <chris@cjwfuller.com>
2014-08-21 22:52:50 +0100

    finish bst inorder traversal without generator for tests

--- python/searching/bst.py
+++ python/searching/bst.py
@@ -1,15 +1,15 @@
+class Node:
+    def __init__(self, data, left=None, right=None):
+        self.left = left
+        self.right = right
+        self.data = data
+
 class BST:
     """Binary Search Tree"""
 
-    def __init__(self, data, left=None, right=None):
-        self.left = None
-        self.right = None
-        self.data = data
-
     def inorder(self, node):
         """Perform in-order traversal of BST"""
-        if node.left is not None:
+        if node is not None:
             self.inorder(node.left)
-        yield node.data
-        if node.right is not None:
+            print node.data
             self.inorder(node.right)
